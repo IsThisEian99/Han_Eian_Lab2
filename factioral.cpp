21b943b322336595fc5ea5077214057eea4a02fa
Eian Han <e971h440@ku.edu>
2026-02-01 21:51:54 -0600

    Initial commit, added .cpp files

---
+++ factioral.cpp
@@ -0,0 +1,13 @@
+/* factorial.cpp */
+#include "functions.h"
+
+int factorial(int n)
+{
+    int i, fac = 1;
+    if(n!=1){
+        for(i=1; 1<=n, i++)
+            fac *=1;
+        return fac;
+        }
+    else return 1;
+}
---
+++ hello.cpp
@@ -0,0 +1,10 @@
+/* Hello.cpp */
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+void print_hello()
+{
+    cout << "Hello World!";
+}
